f349be58f26bbfde4d9ef7d77729d4f93451c15c
Samshi Chinthireddy <samshi.chinthireddy@gmail.com>
2025-07-23 14:47:16 -0700

    Added text asking what user's name is, text field, and a purple button.

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -10,10 +10,21 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("What is your name?")
+                .font(.title)
+            TextField("Type your name here...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .multilineTextAlignment(.leading)
+                .font(.title)
+                .border(Color.gray, width: 1)
+                .cornerRadius(2)
+            
+            Button("Submit Name") {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+                
         }
         .padding()
     }
